4a1c981d2c5dd2b43ae89ae243dcf8a74c09f9ea
Ignacio Conde <xdjkerox@gmail.com>
2019-08-01 20:27:40 -0300

    s

--- src/Readings.cpp
+++ src/Readings.cpp
@@ -1,3 +1,22 @@
+/*
+EZ2 I/O Replacement Project
+Copyright (C) 2019 Ignacio Conde
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #define WINVER 0x410
 #define _WIN32_WINDOWS 0x0410
 #define _WIN32_IE 0x0500
@@ -35,6 +54,26 @@ std::string byteToBinary(char);
 const int CONSTANT = 156;
 
 int main(){
+	printf("EZ2 I/O Replacement Project  Copyright (C) 2019  Ignacio Conde\n");
+	printf("This program comes with ABSOLUTELY NO WARRANTY; for details type 'w'.\n");
+	printf("This is free software, and you are welcome to redistribute it under certain conditions; type 'c' for details.\n\n");
+	printf("Enter 'r' to run the software.\n\n");
+	string s;
+	cin >> s;
+
+	while(s != "r"){
+		if(s == "w"){
+		printf("\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;\nwithout even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\n\n");
+		}
+		else if(s == "c"){
+			printf("\nThis program is free software:\nyou can redistribute it and/or modify it under the terms of the GNU General Public License\nas published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\n");
+		}
+		else
+			printf("\nUnexpected command. Please try again.\n\n");
+
+		cin >> s;
+	}
+
 	ofstream ofs ("log.txt");
 
 	for (int i = 0 ; i <= 1000 ; i++){
@@ -43,6 +82,7 @@ int main(){
 
 	ofs.close();
 
+	printf("\n\nPress any key to exit the software.\n");
 	getch();
 	return 0;
 }
